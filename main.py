53aba9303905dec1eb5cb7ec4402a7cc3bce4535
Yann Wojciechowski <yannwojciech@gmail.com>
2025-06-12 14:17:58 +0200

    Add ball and paddle collision handling with dynamic bounce angles

--- main.py
+++ main.py
@@ -75,6 +75,34 @@ def draw(window, paddles, ball):
     ball.draw(window)
     pygame.display.update()
 
+def handle_collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if ball.y >= left_paddle.y and ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+
+    else:
+        if ball.y >= right_paddle.y and ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+                middle_y = right_paddle.y + right_paddle.height / 2
+                difference_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
+                y_vel = difference_in_y / reduction_factor
+                ball.y_vel = -1 * y_vel
+
 def handle_paddle_movement(keys, left_paddle, right_paddle):
     if keys[pygame.K_s] and left_paddle.y - left_paddle.VELOCITY >= 0: # If your keyboard is Qwerty, replace [pygame.K_s] by [pygame.K_w] here
         left_paddle.move(up=True)
@@ -103,6 +131,9 @@ def main():
                 break
         keys = pygame.key.get_pressed()
         handle_paddle_movement(keys, left_paddle, right_paddle)
+
+        ball.move()
+        handle_collision(ball, left_paddle, right_paddle)
 
     pygame.quit()
 
